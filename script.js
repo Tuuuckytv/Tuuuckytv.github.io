0e56da48a12d21d33a524949d3d3d976e5f988fb
Tuuuckytv <149148727+Tuuuckytv@users.noreply.github.com>
2023-10-27 15:56:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// Konami Code sequence: Up, Up, Down, Down, Left, Right, Left, Right, B, A
-const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
-let konamiCodePosition = 0;
 
-document.addEventListener('keydown', function (event) {
-    if (event.key.toLowerCase() === konamiCode[konamiCodePosition]) {
-        konamiCodePosition++;
-        if (konamiCodePosition === konamiCode.length) {
-            // You can replace this alert with any action you want to trigger.
-            alert('Konami Code activated!');
-            konamiCodePosition = 0; // Reset the position
-        }
-    } else {
-        konamiCodePosition = 0; // Reset if the entered key doesn't match
-    }
-});
