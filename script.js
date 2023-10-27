c0eda4c5b06d3826963bce3feb47f42497f616a8
Tuuuckytv <149148727+Tuuuckytv@users.noreply.github.com>
2023-10-27 15:53:40 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-<script>
-    // Konami Code script
-    const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
-    let konamiIndex = 0;
+// Konami Code sequence: Up, Up, Down, Down, Left, Right, Left, Right, B, A
+const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+let konamiCodePosition = 0;
 
-    document.addEventListener('keydown', (e) => {
-        if (e.key === konamiCode[konamiIndex]) {
-            konamiIndex++;
-
-            if (konamiIndex === konamiCode.length) {
-                alert('Konami Code activated! 🎮');
-                konamiIndex = 0;
-            }
-        } else {
-            konamiIndex = 0;
+document.addEventListener('keydown', function (event) {
+    if (event.key.toLowerCase() === konamiCode[konamiCodePosition]) {
+        konamiCodePosition++;
+        if (konamiCodePosition === konamiCode.length) {
+            // You can replace this alert with any action you want to trigger.
+            alert('Konami Code activated!');
+            konamiCodePosition = 0; // Reset the position
         }
-    });
-</script>
+    } else {
+        konamiCodePosition = 0; // Reset if the entered key doesn't match
+    }
+});
