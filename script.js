ef05a648ad4c0c47f682debe3faeeeb62e4a1bc3
Tuuuckytv <149148727+Tuuuckytv@users.noreply.github.com>
2023-10-27 14:04:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ document.addEventListener("DOMContentLoaded", function () {
         content.style.transform = "scale(1)";
     }, 1000); // Adjust the delay (in milliseconds) as needed
 });
+
+<script>
+    // Konami Code script
+    const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
+    let konamiIndex = 0;
+
+    document.addEventListener('keydown', (e) => {
+        if (e.key === konamiCode[konamiIndex]) {
+            konamiIndex++;
+
+            if (konamiIndex === konamiCode.length) {
+                alert('Konami Code activated! 🎮');
+                konamiIndex = 0;
+            }
+        } else {
+            konamiIndex = 0;
+        }
+    });
+</script>
